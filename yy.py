d669083b5a11efe38686fd1a4a02e42d78ea6c8c
make-7h <mdry94495@gmail.com>
2025-03-10 18:29:04 +0300

    Create yy.py

---
+++ yy.py
@@ -0,0 +1,11 @@
+import requests
+from datetime import datetime, time
+import datetime
+an = datetime.datetime.now()
+an2 = datetime.datetime(2025, 3, 14, 0, 00, 00)
+if an > an2 or an == an2:
+        exit("تم ايقاف الاداة راسل المطوره ريفن - @F_M_D")
+else:
+        pass
+#pretty.install()
+os.system('clear')
